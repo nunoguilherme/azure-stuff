9856380e90f722bce7ecec373a1fc49aa2b8c446
Your Name <your.email@example.com>
2023-06-01 21:59:34 +0100

    Setup Express.js server and SpaceX launches API

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Azure!');
+  res.send('Welcome to MiniCloud SpaceX Launches API!');
+});
+
+app.get('/launches', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.spacexdata.com/v4/launches/latest');
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send('Error occurred while fetching data.');
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
