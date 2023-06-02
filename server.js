8e7845eb3d6c16eeb16f871e65cc5b127b065bb7
Your Name <your.email@example.com>
2023-06-03 00:57:46 +0100

    New endpoint

--- server.js
+++ server.js
@@ -11,15 +11,44 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.get('/launches', async (req, res) => {
+app.get('/launches', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/launch.html'));
+});
+
+app.get('/launch-data', async (req, res, next) => {
   try {
     const response = await axios.get('https://api.spacexdata.com/v4/launches/latest');
-    res.send(response.data);
+    // Extract the necessary data
+    const data = {
+      name: response.data.name,
+      date: response.data.date_utc,
+      details: response.data.details,
+      rocket: response.data.rocket,
+      launchpad: response.data.launchpad,
+      links: response.data.links,
+    };
+    res.json(data);
   } catch (error) {
-    res.status(500).send('Error occurred while fetching data.');
+    next(error);
   }
+});
+
+app.get('/past-launches', async (req, res, next) => {
+  try {
+    const response = await axios.get('https://api.spacexdata.com/v4/launches/past');
+    res.json(response.data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
 
 app.listen(port, () => {
   console.log(`App listening at port number: ${port}`);
 });
+
